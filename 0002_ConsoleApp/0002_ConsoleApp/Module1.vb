81343f2219989405a64413a232613808e99337f9
Israel de la Barrera Lora <isra.lora99@gmail.com>
2020-01-15 09:15:18 +0100

    no message

---
+++ 0002_ConsoleApp/0002_ConsoleApp/Module1.vb
@@ -0,0 +1,15 @@
+﻿Module Module1
+    'Variable de scope globlal
+    Public vari As Integer
+
+
+    'Variable de ambito de clase (funciona en todo el modulo)
+    Dim a As Integer
+
+    Sub main()
+        'Variable local
+        Dim a As Integer
+
+    End Sub
+
+End Module
