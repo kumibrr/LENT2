add70ea22591c9e28f095a5e77f9e5ca311780f0
Israel de la Barrera <isra.lora99@gmail.com>
2020-01-21 20:07:18 +0100

    no message

--- 0003_Primitiva/0003_Primitiva/Module1.vb
+++ 0003_Primitiva/0003_Primitiva/Module1.vb
@@ -1,39 +1,41 @@
 ﻿Module Module1
 
     Sub Main()
-        Dim random As New Random()
+        'Model. This is an array to save the 6 integers
+        Dim numbers(5) As Integer
 
-        Dim ticket(5) As Integer
-        Dim index As Integer = 0
-        Dim ticketDone As Boolean = False
+        'Controller.
+        'Generate 6 random numbers
+
+        Dim index1 = 0
         Randomize()
-
-        Do While index < 6
-
-            ticket(index) = Int((Rnd() * 49) + 1)
-
-
-            Dim isAlready As Boolean = False
+        Do While index1 < 6
+            numbers(index1) = Int(Rnd() * 49) + 1
+            'Check if it is repeated
+            Dim repeated As Boolean = False
             Dim index2 As Integer
-            For index2 = 0 To index - 1
-                If ticket(index) = ticket(index2) Then
-                    isAlready = True
+            For index2 = 0 To index1 - 1
+                If numbers(index1) = numbers(index2) Then
+                    repeated = True
+                    Exit For
                 End If
-                index2 = +1
             Next
-
-            If isAlready = False Then
-                index += 1
+            If repeated = False Then
+                index1 += 1
             End If
 
         Loop
-        Console.WriteLine(ticket(0).ToString)
-        Console.WriteLine(ticket(1).ToString)
-        Console.WriteLine(ticket(2).ToString)
-        Console.WriteLine(ticket(3).ToString)
-        Console.WriteLine(ticket(4).ToString)
-        Console.WriteLine(ticket(5).ToString)
+
+        'For each to show in the numbers in the view.
+        For Each numero As Integer In numbers
+            Dim showNumber As String = numbers.ToString
+            Console.WriteLine(showNumber)
+        Next
+
         Console.ReadLine()
+
+
     End Sub
+
 
 End Module
