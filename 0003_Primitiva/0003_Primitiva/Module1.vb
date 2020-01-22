cca0479c291fd61c44083dc582743558759d66e2
Israel de la Barrera Lora <isra.lora99@gmail.com>
2020-01-22 09:13:59 +0100

    no message

--- 0003_Primitiva/0003_Primitiva/Module1.vb
+++ 0003_Primitiva/0003_Primitiva/Module1.vb
@@ -7,10 +7,10 @@
         'Controller.
         'Generate 6 random numbers
 
-        Dim index1 = 0
+        Dim index1 As Integer = 0
         Randomize()
         Do While index1 < 6
-            numbers(index1) = Int(Rnd() * 49) + 1
+            numbers(index1) = randomInt(1, 49)
             'Check if it is repeated
             Dim repeated As Boolean = False
             Dim index2 As Integer
@@ -26,16 +26,17 @@
 
         Loop
 
-        'For each to show in the numbers in the view.
-        For Each numero As Integer In numbers
-            Dim showNumber As String = numbers.ToString
-            Console.WriteLine(showNumber)
+        'For-each to show the numbers in the view.
+        For Each index As Integer In numbers
+            Console.WriteLine(numbers)
         Next
 
         Console.ReadLine()
-
-
     End Sub
+
+    Function randomInt(min As Integer, max As Integer) As Integer
+        Return Int(Rnd() * max) + min
+    End Function
 
 
 End Module
