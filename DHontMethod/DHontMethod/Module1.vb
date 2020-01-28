b767effcfe0cd50873d3dea392a41f2733dbbd63
Israel de la Barrera Lora <isra.lora99@gmail.com>
2020-01-28 10:08:22 +0100

    no message

---
+++ DHontMethod/DHontMethod/Module1.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+    Const parties As Integer = 5
+
+    Structure Party
+        Dim partyName As String
+        Dim votes As Integer
+        Dim seats As Integer
+        Dim percentage As Decimal
+    End Structure
+
+    Sub CalculatePercentage()
+
+    End Sub
+
+
+    Sub Main()
+
+    End Sub
+
+End Module
