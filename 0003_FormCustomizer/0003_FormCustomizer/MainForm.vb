e5a484fadfd19a5df50b391a150fadf9a195cc77
Israel de la Barrera Lora <isra.lora99@gmail.com>
2020-02-25 10:17:01 +0100

    no message

---
+++ 0003_FormCustomizer/0003_FormCustomizer/MainForm.vb
@@ -0,0 +1,46 @@
+﻿Public Class MainForm
+    Inherits Form
+
+    Private lblFormTitle As Label
+    Private txtFormtitle As TextBox
+    Private lblFormTxtBox As Label
+    Private txtFormTxtBox As TextBox
+    Private lblButton1 As Label
+    Private txtButton1 As TextBox
+    Private lblButton2 As Label
+    Private txtButton2 As TextBox
+    Private lblButton3 As Label
+    Private txtButton3 As TextBox
+    Private btnModal As Button
+    Private btnNotModal As Button
+    Private btnExit As Button
+
+
+    Public Sub New()
+        MyBase.New()
+        Me.Size = New Size(400, 300)
+
+        lblFormTitle = New Label
+        lblFormTitle.Text = "Window title"
+        lblFormTitle.Location = New Point(15, 10)
+        lblFormTitle.Size = New Size(100, 20)
+
+        txtFormtitle = New TextBox
+        txtFormtitle.Location = New Point(150, 8)
+        txtFormtitle.Size = New Size(200, 20)
+
+        lblFormTxtBox = New Label
+        lblFormTxtBox.Text = "Text inside the window"
+        lblFormTxtBox.Size = New Size(100, 40)
+        lblFormTxtBox.Location = New Point(15, 40)
+
+        txtFormTxtBox = New TextBox
+        txtFormTxtBox.Size = New Size(132, 20)
+        txtFormTxtBox.Location = New Point(120, 45)
+
+        Me.Controls.Add(lblFormTitle)
+        Me.Controls.Add(txtFormtitle)
+        Me.Controls.Add(lblFormTxtBox)
+        Me.Controls.Add(txtFormTxtBox)
+    End Sub
+End Class
